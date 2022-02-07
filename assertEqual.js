eb693ac2330e727bb25d25a2cc5dd1c733b53381
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-07 15:20:35 -0500

    Created an assertion function to test if two arguments were equal to each other

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❌❌❌ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Code
+
+assertEqual("Hello", "Hello");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 17);
