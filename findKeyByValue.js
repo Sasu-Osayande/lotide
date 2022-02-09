6cabc91f8af3b4069113c200f41eb2ff7cb2a11a
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-09 18:24:08 -0500

    Implemented a function that returns the key from an objects where its value matches a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+
+//Test Code
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const dishesInCuisine = { 
+  mexican: "tacos",
+  thai: "pad thai",
+  italian: "spaghetti"
+};
+
+assertEqual(findKeyByValue(dishesInCuisine, "pad thai"), "thai");
+assertEqual(findKeyByValue(dishesInCuisine, "tamales"), undefined);
