7da635bd4a490b8d54ce4b8c7a181272f651f363
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-10 18:47:43 -0500

    Implemented our own map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray.length !== secondArray.length) {
+      return false;
+    }
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray) === false) {
+    console.log(`❌❌❌ Assertion failed: ${firstArray} != ${secondArray}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${firstArray} = ${secondArray}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results1.length, 5);
+assertArraysEqual(Array.isArray(results1), true)
