6d1308263a347ea8403e7d3ed3a625f44b0f451d
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-10 19:55:10 -0500

    Implemented a function that takes an object and a callback and returns the first key for which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const data1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+// console.log(data1);
+assertEqual(data1, "noma");
+
+const data2 = findKey({
+  tomatoes: 2,
+  mangoes: 5,
+  oranges: 2,
+  pears: 4,
+  mushrooms: 10,
+  grapes: 2
+}, x => x === 4);
+
+// console.log(data2);
+assertEqual(data2, "pears");
