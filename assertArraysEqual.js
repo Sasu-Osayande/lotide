6b4d0b972d082548060fb3fb8f1d4ce3aa8e0d33
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-14 13:17:48 -0500

    Refactored by exporting the function and removing the code for eqArrays and requiring it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray.length !== secondArray.length) {
-      return false;
-    }
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray) === false) {
@@ -19,3 +9,5 @@ const assertArraysEqual = function(firstArray, secondArray) {
 };
 
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+mmodule.exports = assertArraysEqual;
