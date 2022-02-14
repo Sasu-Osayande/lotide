f83f995f3fbe81560bc27a6c9de942545f91aa62
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-14 16:53:25 -0500

    created an object to export that contains all of the functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countOnly,
+  countLetters,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+};
