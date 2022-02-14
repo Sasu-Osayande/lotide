1131a6e101a77a4487fe9376bd287aa232a4c73f
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-14 10:09:06 -0500

    Made the code more DRY by moving the test code to another file

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   if (array.length > 1) {
     return array.slice(1);
@@ -13,20 +5,4 @@ const tail = function(array) {
   return [];
 };
 
-
-// Test Code
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const emptyTail = tail(["Hey"]);
-assertEqual(tail(emptyTail).length, 0);
-
-const emptyArray = tail([]);
-assertEqual(tail(emptyArray).length, 0);
+module.exports = tail;
